5154d0356051a4e6e6f9b532e8ce50e59696bd64
Zhivko Draganov <zhivko.draganov@gmail.com>
2015-12-26 18:50:57 +0200

    Finish track event class

--- lib/client/event_queue.rb
+++ lib/client/event_queue.rb
@@ -17,24 +17,57 @@ class EventQueue
   end
 
   def search(query, result_count)
+    data = {
+      query: query, result_count: result_count
+    }
+
+    add_event(:track, :search, data)
   end
 
-  def page_view
+  def page_view(title, backend_hook = nil)
+    add_event(:track, :pageview, title, backend_hook: backend_hook)
   end
 
-  def view_category
+  def view_category(category_id, category_name)
+    data = { id: category_id, name: category_name }
+
+    add_event(:track, :view_category, data)
   end
 
-  def view_product
+  def view_product(
+    product_id, product_name, product_price, product_url, options = {}
+  )
+    data = {
+      id: product_id, name: product_name, price: product_price, url: product_url
+    }
+
+    data[:image_url]  = options[:image_url] if options.key?(:image_url)
+    data[:categories] = options[:categories] if options.key?(:categories)
+
+    add_event(:track, :view_product, data)
   end
 
   def view_cart
+    add_event(:track, :view_cart)
   end
 
-  def add_to_cart
+  def add_to_cart(
+    product_id, product_name, product_price, product_url, options = {}
+  )
+    data = {
+      id: product_id, name: product_name, price: product_price, url: product_url
+    }
+
+    data[:quantity]     = options.key?(:quantity) ? options[:quantity] : 1
+    data[:option_id]    = options[:option_id] if options.key?(:option_id)
+    data[:option_name]  = options[:option_name] if options.key?(:option_name)
+    data[:option_price] = options[:option_price] if options.key?(:option_price)
+
+    data
   end
 
   def checkout_start
+    add_event(:track, :checkout_start)
   end
 
   private
@@ -43,7 +76,8 @@ class EventQueue
     event = {
       method: method,
       type: type,
-      data: data.merge(meta_data: meta_data)
+      data: data,
+      meta_data: meta_data
     }
 
     @tracked_events << event
